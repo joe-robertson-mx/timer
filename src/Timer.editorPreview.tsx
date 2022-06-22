99a5fdab26ab33cd93b1f13ab655bd16fb9ab40c
Joe Robertson <32719006+joerob319@users.noreply.github.com>
2022-06-22 15:35:54 +0100

    Removed editorPreview

--- src/Timer.editorPreview.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement, createElement } from "react";
-
-export function preview(): ReactElement {
-    return <div />;
-}
-
-export function getPreviewCss(): string {
-    return require("./ui/Timer.css");
-}
